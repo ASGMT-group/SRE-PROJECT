7acb85a7374b99782463ab88b6ecb058889c0036
wondmD <wondmenehdereje@gmail.com>
2023-05-30 05:37:40 +0300

    published registrations page

--- Curriculum360/users/views.py
+++ Curriculum360/users/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
-
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request , ("Succussfully Registerd !"))
+            return redirect('dashboard')
+    else :
+        form = UserCreationForm()
+    return render (request, 'register_user.html' , {'form':form})
 def login_user(request):
     if request.method == 'POST':
         username = request.POST["username"]
